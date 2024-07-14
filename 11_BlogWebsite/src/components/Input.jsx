189c4a2e34ce64f85d8a2319c9a2c142a8fc2804
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-07-14 23:41:17 +0500

    Components are ready now for usage!

--- 11_BlogWebsite/src/components/Input.jsx
+++ 11_BlogWebsite/src/components/Input.jsx
@@ -4,6 +4,7 @@ const Input = forwardRef(function Input(
   { label, type = "text", className = "", ...props },
   ref
 ) {
+  const id = useId()
   return (
     <div className="w-full">
       {label && (
